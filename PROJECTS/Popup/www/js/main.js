6d533ae0603a26028e2f7da6d4f7dd5ab4084ff2
SJ <1488belial666@gmail.com>
2015-08-01 12:44:09 +0300

    update(animation) V 1.1

--- PROJECTS/Popup/www/js/main.js
+++ PROJECTS/Popup/www/js/main.js
@@ -1,10 +1,11 @@
     $(document).ready(function(){
         //# Скрыть PopUp при загрузке страницы    
-        PopUpHide();
+        $(".popup").hide();
     });
     //# Функция отображения PopUp c выводом значения выбора пользователя
     function PopUpShow(){
-        $(".popup").show();
+		var sh =$(".popup").fadeIn(300);
+
 		function no() {
 		PopUpHide();
 		return console.log('false');
@@ -20,7 +21,42 @@
 	//# Првязываем событие к элементу документа
 	$('.container').on('click',PopUpShow);
 	
+	
     //# Функция скрытия PopUp Закрываем при нажатии на кнопку
     function PopUpHide(){
+        $(".popup").fadeOut(200);
+    };
+	//=================================================
+/* var popup = {
+show:function PopUpShow(){
+        $(".popup").show();
+		function no() {
+		PopUpHide();
+		return console.log('false');
+		};
+		function yes() {
+		PopUpHide();
+		return console.log('true');
+		};
+		$('.no').on('click',no);
+		$('.yes').on('click',yes);
+		return (yes || no);
+    },
+hide:function PopUpHide(){
         $(".popup").hide();
-    }
+    },
+addEventBtn: function(event, fn){},
+appendTo: function(selector){}
+};
+var helper = {
+$: function(selector, context){
+return (context || document).querySelectorAll(selector);
+},
+hide: function(elm){
+elm.style.display = 'none';
+}.
+show: function (elm){
+elm.style.display = 'block';
+}
+};
+helper.$('.block'); */
